42712d719d9746f89478f67c54bb422ec6891f26
Niv Adam <nivadam@web.de>
2023-10-19 19:41:02 +0200

    add two-opt wip

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,34 @@
 mod utils;
+mod test;
 
 use std::io::{self};
-use crate::utils::Graph;
+use std::time::Instant;
+use crate::utils::{euclidean_distance, Graph};
 use crate::utils::SparseGraph;
 
-// fn nearest_neighbor_tour(points: &Vec<(f64, f64)>) -> Vec<usize> {
-//     let n = points.len();
-//     let mut tour = vec![0; n];
-//     let mut used = vec![false; n];
-//
-//     used[0] = true;
-//
-//     for i in 1..n {
-//         let mut best = None;
-//         for j in 0..n {
-//             if !used[j] && (best.is_none() || utils::euclidean_distance(points[tour[i-1]], points[j]) < utils::euclidean_distance(points[tour[i-1]], points[best.unwrap()])) {
-//                 best = Some(j);
-//             }
-//         }
-//         tour[i] = best.unwrap();
-//         used[tour[i]] = true;
-//     }
-//
-//     tour
-// }
+fn nearest_neighbor_tour(points: &Vec<(f64, f64)>) -> Vec<i32> {
+    let n = points.len();
+    let mut tour:Vec<i32> = vec![0; n];
+    let mut used = vec![false; n];
+
+    used[0] = true;
+
+    for i in 1..n {
+        let mut best = None;
+        for j in 0..n {
+            if !used[j] && (best.is_none() || utils::euclidean_distance(points[tour[i-1] as usize], points[j]) < utils::euclidean_distance(points[tour[i-1] as usize], points[best.unwrap()])) {
+                best = Some(j);
+            }
+        }
+        tour[i] = best.unwrap() as i32;
+        used[tour[i] as usize] = true;
+    }
+
+    tour
+}
 
 fn greedy_tour(graph: &Graph) -> Vec<i32> {
+    let start_time = Instant::now();
     let n = graph.num_nodes;
 
     let mut sorted = graph.get_edges_sorted();
@@ -49,6 +52,36 @@ fn greedy_tour(graph: &Graph) -> Vec<i32> {
         let cur = sparse_graph.get_neighbors(tour[i-1]);
         tour[i] = if i > 1 && cur[1] != tour[i-2] {cur[1]} else {cur[0]};
     }
+
+    // two-opt
+    // let mut improved = true;
+    // let mut iterations = 0;
+    // let max_iterations = 5;
+    //
+    // while improved /* && iterations < max_iterations */ && start_time.elapsed().as_millis() < 1800 {
+    //     improved = false;
+    //     for i in 0..tour.len() - 1 {
+    //         for j in i + 2..tour.len() {
+    //             if j != i && j != i + 1 {
+    //
+    //                 let old_1 = sparse_graph.get_neighbors(tour[i]).iter().find(|&&(a, _)| a == tour[i+1]);
+    //                 let old_2 = sparse_graph.get_neighbors(tour[j]).iter().find(|&&(a, _)| a == tour[(j + 1) % tour.len()]);
+    //                 let old_dist = old_1.unwrap().1 + old_2.unwrap().1;
+    //
+    //                 let new_1 = sparse_graph.get_neighbors(tour[i]).iter().find(|&&(a, _)| a == tour[j]);
+    //                 let new_2 = sparse_graph.get_neighbors(tour[i+1]).iter().find(|&&(a, _)| a == tour[(j + 1) % tour.len()]);
+    //                 let new_dist = new_1.unwrap().1 + new_2.unwrap().1;
+    //                 tour[i + 1..=j].reverse();
+    //
+    //                 if new_dist < old_dist {
+    //                     tour[i + 1..=j].reverse();
+    //                     improved = true;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     iterations += 1;
+    // }
 
     tour
 }
@@ -80,8 +113,14 @@ fn main() {
     //     }
     // }
 
-    let result = greedy_tour(&graph);
-    for res in result {
+    let tour = greedy_tour(&graph);
+
+
+
+    // output
+    for res in tour {
         println!("{:?}", res);
     }
+
+
 }
